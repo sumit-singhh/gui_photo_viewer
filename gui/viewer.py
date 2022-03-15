533ae969c7028b61a319c20036068b6a854f3231
sumit-singhh <88222854+sumit-singhh@users.noreply.github.com>
2022-03-15 09:49:47 +0530

    Update viewer.py

--- gui/viewer.py
+++ gui/viewer.py
@@ -13,8 +13,12 @@ for i in range(0, 5):
     image = ImageTk.PhotoImage(Image.open(img))
     imageList.append(image)
 
+
 label = Label(root, image=imageList[0])
 label.grid(row=0, column=0, columnspan=3)
+
+status = Label(root, text=f'Image 1 of 5')
+status.grid(row=2, column=2, columnspan=3)
 
 def back(number):
     global label
@@ -30,6 +34,10 @@ def back(number):
         label.grid(row=0, column=0, columnspan=3)
         backButton.grid(row=1, column=0)
         nextButton.grid(row=1, column=2)
+
+        status = Label(root, text=f'Image {number} of 5')
+        status.grid(row=2, column=2, columnspan=3)
+
     else: pass
 
 def next(number):
@@ -48,6 +56,9 @@ def next(number):
         label.grid(row=0, column=0, columnspan=3)
         backButton.grid(row=1, column=0)
         nextButton.grid(row=1, column=2)
+
+        status = Label(root, text=f'Image {number} of 5')
+        status.grid(row=2, column=2, columnspan=3)
 
 
 backButton = Button(root, text='<<', borderwidth=4, command=lambda: back())
